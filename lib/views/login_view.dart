4f3e5766601203eecac922a8376407639c93cc8b
unknown <hornspill@gmail.com>
2024-03-16 23:31:23 +0200

    add error dialog

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:privatenotes/constants/constants.dart';
+import 'package:privatenotes/utilities/show_error_dialog.dart';
 
 class LoginView extends StatefulWidget {
   const LoginView({super.key});
@@ -120,8 +121,14 @@ class _LoginViewState extends State<LoginView> {
                       );
                     } on FirebaseAuthException catch (e) {
                       if (e.code == 'user-not-found') {
-                        print('not found');
+                        await showErrorDialog(context, 'User not found');
+                      } else if (e.code == 'wrong-password') {
+                        await showErrorDialog(context, 'Wrong credentials');
+                      } else {
+                        await showErrorDialog(context, 'Error: ${e.code}');
                       }
+                    } catch (e) {
+                      await showErrorDialog(context, e.toString());
                     }
                   },
                   child: const Padding(
@@ -146,3 +153,4 @@ class _LoginViewState extends State<LoginView> {
     );
   }
 }
+
