134dc7a29129523c26a3df438b847ade801eccd4
andrew <hornespill@gmail.com>
2024-03-26 23:03:55 +0100

    working on auth provider

---
+++ lib/services/auth/firebase_auth_provide.dart
@@ -0,0 +1,8 @@
+import 'package:privatenotes/services/auth/auth_user.dart';
+import 'package:privatenotes/services/auth/auth_provider.dart';
+import 'package:privatenotes/services/auth/auth_exeptions.dart';
+import 'package:firebase_auth/firebase_auth.dart' show FirebaseAuth, FirebaseAuthException;
+
+class FirebaseAuthProvider implements AuthProvider{
+
+}
